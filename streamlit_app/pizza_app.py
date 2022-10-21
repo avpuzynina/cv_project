263a9360b882f7f451b81ec1d31a2601fb0d0822
avpuzynina <a.v.puzynina@gmail.com>
2022-10-21 09:45:35 +0300

    this page done

--- streamlit_app/pizza_app.py
+++ streamlit_app/pizza_app.py
@@ -11,7 +11,7 @@ from torchvision import transforms as T
 from torchvision import io
 import numpy as np
 import matplotlib.pyplot as plt
-
+from torchvision import transforms
 # print("Sreamlit version", st.__version__)
 
 
@@ -21,8 +21,8 @@ def load_image(image_file):
 
 
 def main():
-    st.title("File Upload Tutorial")
-    image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+    st.title("Пицца или не пицца?")
+    image_file = st.file_uploader("Загрузите изображение", type=["png","jpg","jpeg"])
     if image_file is not None:
     #     file_details = {"filename":image_file.name, "filetype":image_file.type,
     #                     "filesize":image_file.size}
@@ -32,19 +32,26 @@ def main():
         # resize = T.Resize((224, 224))
         # print('Type':, type(image_file))
         dictionar_class = {
-            0: 'Не пицца',
-            1: 'Пицца'}
+            0: 'не пицца',
+            1: 'пицца'}
 
         decode = lambda x: dictionar_class[int(x)] 
+        convert_tensor = transforms.ToTensor()
 
         resize = T.Resize((224, 224))
-        img = resize(load_image(image_file)/255)
-        model = torch.load_state_dict(torch.load('/home/anna/cv_project/model_pizza.pt', map_location=torch.device('cpu')))
+        img = resize(convert_tensor(load_image(image_file)))
+        print(type(img))
+
+        model = torch.hub.load("pytorch/vision:v0.10.0", "resnet18")
+        model.fc = nn.Linear(512, 1)
+        model.load_state_dict(torch.load('/home/anna/cv_project/model_pizza.pt', map_location=torch.device('cpu')))
+
         model.eval()
         # plt.imshow(torch.permute(img, (1, 2, 0)))
         label = decode(torch.round(model(img.unsqueeze(0)).sigmoid()).item())
+        st.title('Возможно, это ' + label)
         st.image(load_image(image_file), width=250)
-        st.title(label)
+        # st.title(label)
         
 
 if __name__ == "__main__":
